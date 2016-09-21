43e1fcb0b25e8b4b75e811b9771e084e7f6b7bd4
Jenny Kim <jennykim@cloudera.com>
2016-09-21 18:36:12 -0400

    HUE-4931 [fb] Fix parquet decoding error on debug log messages

--- desktop/core/ext-py/parquet-1.1/parquet/__init__.py
+++ desktop/core/ext-py/parquet-1.1/parquet/__init__.py
@@ -452,7 +452,7 @@ class JsonWriter(object):
     def writerow(self, row):
         json_text = json.dumps(row)
         if type(json_text) is bytes:
-            json_text = json_text.decode('utf-8')
+            json_text = json_text.decode('utf-8', errors='ignore')
         self._out.write(json_text)
         self._out.write(u'\n')
 
@@ -475,7 +475,7 @@ def _dump(fo, options, out=sys.stdout):
             writer.writeheader()
         if options.limit != -1 and total_count >= options.limit:
             return
-        row_unicode = dict((k, (v.decode("utf-8") if type(v) is bytes else v)) for k, v in row.items())
+        row_unicode = dict((k, (v.decode("utf-8", errors="ignore") if type(v) is bytes else v)) for k, v in row.items())
         writer.writerow(row_unicode)
         total_count += 1
 
